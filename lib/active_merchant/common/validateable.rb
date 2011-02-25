297ce167ed2b46caf3c74c802a0b7793391164ce
tinogomes <tinorj@gmail.com>
2011-02-25 10:51:06 -0300

    adding I18n for errors on "models"

--- lib/active_merchant/common/validateable.rb
+++ lib/active_merchant/common/validateable.rb
@@ -1,4 +1,4 @@
-module ActiveMerchant #:nodoc:  
+module ActiveMerchant #:nodoc:
   module Validateable #:nodoc:
     def valid?
       errors.clear
@@ -7,13 +7,13 @@ module ActiveMerchant #:nodoc:
       validate if respond_to?(:validate, true)
 
       errors.empty?
-    end  
+    end
 
     def initialize(attributes = {})
       self.attributes = attributes
     end
 
-    def errors    
+    def errors
       @errors ||= Errors.new(self)
     end
 
@@ -22,24 +22,31 @@ module ActiveMerchant #:nodoc:
     def attributes=(attributes)
       unless attributes.nil?
         for key, value in attributes
-          send("#{key}=", value )            
+          send("#{key}=", value )
         end
       end
-    end  
+    end
 
     # This hash keeps the errors of the object
     class Errors < HashWithIndifferentAccess
 
+      DEFAULT_ERROR_MESSAGES = {
+        :blank => "cannot be blank",
+        :empty => "cannot be empty",
+        :invalid => "is invalid",
+        :required => "is required"
+      }
+
       def initialize(base)
         @base = base
       end
-      
+
       def count
         size
       end
 
-      # returns a specific fields error message. 
-      # if more than one error is available we will only return the first. If no error is available 
+      # returns a specific fields error message.
+      # if more than one error is available we will only return the first. If no error is available
       # we return an empty string
       def on(field)
         self[field].to_a.first
@@ -47,21 +54,21 @@ module ActiveMerchant #:nodoc:
 
       def add(field, error)
         self[field] ||= []
-        self[field] << error
-      end    
-      
+        self[field] << message_error(error)
+      end
+
       def add_to_base(error)
         add(:base, error)
       end
 
       def each_full
-        full_messages.each { |msg| yield msg }      
+        full_messages.each { |msg| yield msg }
       end
 
       def full_messages
         result = []
 
-        self.each do |key, messages| 
+        self.each do |key, messages|
           if key == 'base'
             result << "#{messages.first}"
           else
@@ -70,7 +77,32 @@ module ActiveMerchant #:nodoc:
         end
 
         result
-      end   
-    end  
+      end
+
+      private
+      # returns a specific message error for <tt>error</tt> attribute
+      # <tt>error</tt>: A string message or symbol. If it is a string,
+      # returns it as message error. If it is a symbol, try to use it
+      # as a key on I18n.translation with specific scope from class or
+      # "active_merchant.errors" as default. If translation not exist,
+      # use DEFAULT_ERROR_MESSAGES messages or stringify the symbol and
+      # use it as message error
+      #
+      # E.g:
+      #   # on ActiveMerchant::Billing::CreditCard model
+      #   some_credit_card_instance.errors.add(:number, :invalid)
+      #
+      # That will try to get message:
+      # 1) on I18n translations by key: active_merchant.billing.credit_card.errors.invalid
+      # 2) on I18n translations by key: with key active_merchant.errors.invalid
+      # 3) DEFAULT_ERROR_MESSAGES[invalid]
+      # 4) So, :invalid.to_s.humanize.downcase
+      def message_error(error)
+        return error if error.is_a?(String)
+        klass = @base.class.name.gsub("::", ".").underscore.downcase
+        default_message_error = I18n.t(error, :scope => "active_merchant.errors", :default => DEFAULT_ERROR_MESSAGES[error] || error.to_s.humanize.downcase)
+        I18n.t(error, :scope => "#{klass}.errors", :default => default_message_error)
+      end
+    end
   end
 end
--- test/unit/validateable_test.rb
+++ test/unit/validateable_test.rb
@@ -1,45 +1,53 @@
 require 'test_helper'
 
-class Dood
-  include ActiveMerchant::Validateable
+module ActiveMerchant #:nodoc:
+  module Billing #:nodoc:
+    class Dood
+      include ActiveMerchant::Validateable
 
-  attr_accessor :name, :email, :country
+      attr_accessor :name, :email, :country
 
-  def validate
-    errors.add "name", "cannot be empty" if name.blank?
-    errors.add "email", "cannot be empty" if email.blank?
-    errors.add_to_base "The country cannot be blank" if country.blank?
+      def validate
+        errors.add "name", :empty if name.blank?
+        errors.add "email", "cannot be empty" if email.blank?
+        errors.add_to_base "The country cannot be blank" if country.blank?
+      end
+    end
   end
-
 end
 
 class ValidateableTest < Test::Unit::TestCase
   include ActiveMerchant
-  
+
   def setup
     @dood = Dood.new
+    @current_locale = I18n.locale
   end
 
-  def test_validation  
+  def teardown
+    I18n.locale = @current_locale
+  end
+
+  def test_validation
     assert ! @dood.valid?
     assert ! @dood.errors.empty?
   end
 
-  def test_assigns 
+  def test_assigns
     @dood = Dood.new(:name => "tobi", :email => "tobi@neech.de", :country => 'DE')
 
-    assert_equal "tobi", @dood.name 
+    assert_equal "tobi", @dood.name
     assert_equal "tobi@neech.de", @dood.email
     assert @dood.valid?
   end
 
   def test_multiple_calls
-    @dood.name = "tobi"        
-    assert !@dood.valid?    
-    
-    @dood.email = "tobi@neech.de"    
+    @dood.name = "tobi"
     assert !@dood.valid?
-    
+
+    @dood.email = "tobi@neech.de"
+    assert !@dood.valid?
+
     @dood.country = 'DE'
     assert @dood.valid?
   end
@@ -57,4 +65,46 @@ class ValidateableTest < Test::Unit::TestCase
     assert_equal ["Email cannot be empty", "Name cannot be empty", "The country cannot be blank"], @dood.errors.full_messages.sort
   end
 
+  def test_default_error_messages_for_blank
+    @dood.errors.add(:name, :blank)
+    assert_equal "cannot be blank", @dood.errors.on(:name)
+  end
+
+  def test_default_error_messages_for_empty
+    @dood.errors.add(:name, :empty)
+    assert_equal "cannot be empty", @dood.errors.on(:name)
+  end
+
+  def test_default_error_messages_for_invalid
+    @dood.errors.add(:name, :invalid)
+    assert_equal "is invalid", @dood.errors.on(:name)
+  end
+
+  def test_default_error_messages_for_required
+    @dood.errors.add(:name, :required)
+    assert_equal "is required", @dood.errors.on(:name)
+  end
+
+  def test_error_message_for_blank_with_translation
+    I18n.backend.store_translations :en, :active_merchant => {:errors => {:blank => "blank is invalid"}}
+    I18n.backend.store_translations :pt, :active_merchant => {:errors => {:blank => "deve ser preenchido"}}
+
+    @dood.errors.add(:name, :blank)
+    assert_equal "blank is invalid", @dood.errors.on(:name)
+
+    I18n.locale = :pt
+
+    @dood.errors.clear
+    @dood.errors.add(:name, :blank)
+    assert_equal "deve ser preenchido", @dood.errors.on(:name)
+  end
+
+  def test_error_message_translated_using_priority_order
+    I18n.backend.store_translations :pt, :active_merchant => {:billing => { :dood => {:errors => {:blank => "nunca deve estar em branco"}}}}
+
+    I18n.locale = :pt
+    @dood.errors.clear
+    @dood.errors.add(:name, :blank)
+    assert_equal "nunca deve estar em branco", @dood.errors.on(:name)
+  end
 end
